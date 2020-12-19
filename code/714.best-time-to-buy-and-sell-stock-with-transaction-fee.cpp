7b277d73cd0ce469ac33d11ed5ee57afa617641e
oldw4ng <ruizewang96@gmail.com>
2020-12-19 16:49:29 +0800

    2020-12-19

---
+++ code/714.best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode id=714 lang=cpp
+ *
+ * [714] Best Time to Buy and Sell Stock with Transaction Fee
+ *
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/description/
+ *
+ * algorithms
+ * Medium (55.63%)
+ * Likes:    1930
+ * Dislikes: 59
+ * Total Accepted:    81.6K
+ * Total Submissions: 146.7K
+ * Testcase Example:  '[1,3,2,8,4,9]\n2'
+ *
+ * Your are given an array of integers prices, for which the i-th element is
+ * the price of a given stock on day i; and a non-negative integer fee
+ * representing a transaction fee.
+ * You may complete as many transactions as you like, but you need to pay the
+ * transaction fee for each transaction.  You may not buy more than 1 share of
+ * a stock at a time (ie. you must sell the stock share before you buy again.)
+ * Return the maximum profit you can make.
+ * 
+ * Example 1:
+ * 
+ * Input: prices = [1, 3, 2, 8, 4, 9], fee = 2
+ * Output: 8
+ * Explanation: The maximum profit can be achieved by:
+ * Buying at prices[0] = 1Selling at prices[3] = 8Buying at prices[4] =
+ * 4Selling at prices[5] = 9The total profit is ((8 - 1) - 2) + ((9 - 4) - 2) =
+ * 8.
+ * 
+ * 
+ * 
+ * Note:
+ * 0 < prices.length .
+ * 0 < prices[i] < 50000.
+ * 0 .
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int ret = 0, pay = -prices[0];
+        for (int i = 1; i < prices.size(); ++i) {
+            ret = max(ret, pay + prices[i] - fee);
+            pay = max(pay, ret - prices[i]);
+        }
+        return ret;
+    }
+};
+// @lc code=end
+
